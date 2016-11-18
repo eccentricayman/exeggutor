98fe64f87104ca6178adefc17ce8287485fa5e45
eccentricayman <aahmed3@stuy.edu>
2016-11-18 15:38:10 -0500

    fixing the \n

--- executor.c
+++ executor.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 /*
 Prompts the user for a program to run via the command line.
@@ -9,6 +10,36 @@ Runs the command using exec
 You do not need to fork off a separate process and wait, the purpose of this assignment is only to parse a single line of input and run a program once.
 */
 
+void execString(char toExec[]) {
+
+    char *arg = toExec;
+    char * command[strlen(toExec)];
+
+    int i = 0;
+    while (arg) {
+        command[i] = strsep(&arg, " ");
+        printf("%s\n", command[i]);
+        i++;
+    }
+    command[i] = 0;
+    execvp(command[0], command);
+}
+
 int main() {
+    //max input length for mac terminal.app
+    char input[262144];
     
+    printf("wyd fam: ");
+    fgets(input, 262144, stdin);
+
+    char filteredInput[262144];
+    for (int i = 0 ; i < strlen(input) - 1 ; i++) {
+        
+    }
+
+    
+    printf("a%sb", input);
+    //execString(input);
+
+    return 0;
 }
